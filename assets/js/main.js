6d3b3c2d85b7ff799b9c0304ee31d23ffa37466b
giuliano <giuliano.mesias@hotmail.com>
2026-02-10 21:48:13 -0500

    hero and style

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,5 +6,15 @@ function loadComponent(id, file) {
     });
 }
 
+window.addEventListener("scroll", () => {
+  const nav = document.querySelector(".nav");
+
+  if (window.scrollY > 50) {
+    nav.classList.add("scrolled");
+  } else {
+    nav.classList.remove("scrolled");
+  }
+});
+
 loadComponent("navbar", "/partials/navbar.html");
 loadComponent("footer", "/partials/footer.html");
