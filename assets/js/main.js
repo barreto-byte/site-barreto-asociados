3130cbb21655767df2221e4430ca2ec1695ea833
giuliano <giuliano.mesias@hotmail.com>
2026-02-10 23:51:11 -0500

    correccion navbar 2.7

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,21 @@
-function loadComponent(id, file) {
+function loadComponent(id, file, callback) {
   fetch(file)
     .then(res => res.text())
     .then(data => {
       document.getElementById(id).innerHTML = data;
+      if (callback) callback();
     });
 }
-  const nav = document.querySelector(".nav");
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 50) {
-    nav.classList.add("scrolled");
-  } else {
-    nav.classList.remove("scrolled");
-  }
+loadComponent("navbar", "/partials/navbar.html", () => {
+  const navbar = document.querySelector(".navbar");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+      navbar.classList.add("scrolled");
+    } else {
+      navbar.classList.remove("scrolled");
+    }
+  });
 });
 
-loadComponent("navbar", "/partials/navbar.html");
 loadComponent("footer", "/partials/footer.html");
